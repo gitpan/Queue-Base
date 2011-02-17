a187a04677da5da6a72120ba02716c7a36fd2643
Alexei Znamensky <russoz@cpan.org>
2011-02-17 00:07:00 +0000

    Import of RUSSOZ/Queue-Base-2.0_5 from CPAN.
    gitpan-cpan-distribution: Queue-Base
    gitpan-cpan-version:      2.0_5
    gitpan-cpan-path:         RUSSOZ/Queue-Base-2.0_5.tar.gz
    gitpan-cpan-author:       RUSSOZ
    gitpan-cpan-maturity:     developer

--- lib/Queue/Base.pm
+++ lib/Queue/Base.pm
@@ -2,7 +2,7 @@
 package Queue::Base;
 
 BEGIN {
-    $Queue::Base::VERSION = '2.0_4';
+    $Queue::Base::VERSION = '2.0_5';
 }
 use strict;
 use warnings;
@@ -74,7 +74,7 @@ Queue::Base - Simple OO style queue implementation.
 
 =head1 VERSION
 
-version 2.0_4
+version 2.0_5
 
 =head1 SYNOPSIS
 
